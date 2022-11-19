f97d8a83a8e162135871d9de03dc6cd211e43e43
Mihaly Kisvarday <m.kisvarday@codat.io>
2022-11-19 17:39:45 +0000

    Add project files.

---
+++ CodingChallanges.Tests/CollectionsTests.cs
@@ -0,0 +1,19 @@
+﻿using CodingChallanges.Collections;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingChallanges.Tests;
+
+[TestClass]
+public class CollectionsTests
+{
+    [TestMethod]
+    [DataRow(new[] { -1, -2, -3, -4, -5 }, new[] { 1, 2, 3, 4, 5 })]
+    [DataRow(new[] { -1, 2, -3, 4, -5 }, new[] { 1, -2, 3, -4, 5 })]
+    [DataRow(new int[] { }, new int[] { })]
+    [DataRow(new[] { 0 }, new[] { 0 })]
+    public void ArrayInversion_ReturnsExpectedResult(int[] input, int[] expectedOutput)
+    {
+        ArraysInversion.InvertValues(input).Should().BeEquivalentTo(expectedOutput);
+    }
+}
---
+++ CodingChallanges/Collections/ArraysInversion.cs
@@ -0,0 +1,21 @@
+﻿namespace CodingChallanges.Collections;
+
+/*
+
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+Examples:
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+
+ */
+public static class ArraysInversion
+{
+    public static int[] InvertValues(int[] input)
+    {
+        //Code it!
+        throw new NotImplementedException();
+    }
+}
